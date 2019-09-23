81d00febedf563811c084a94d389fc8b8a9371b3
Onur Gumus <onur.gumus@emaratech.ae>
2019-09-23 07:34:17 +0400

    Lessons 7 and 8

---
+++ Section7/EverythingPublic/EverythingPublic/Program.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EverythingPublic
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; private set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=blogging.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            //choose one below
+            modelBuilder.Entity<Blog>()
+            .Property(p => p.Url)
+            .IsConcurrencyToken();
+
+            modelBuilder.Entity<Blog>()
+           .Property(p => p.Timestamp)
+           .IsRowVersion();
+
+            modelbuilder.HasSequence<int>("DBSequenceHiLo").StartsAt(1000).IncrementsBy(5);
+            modelbuilder.ForSqlServerUseSequenceHiLo("DBSequenceHiLo");
+        }
+
+    }
+
+    public class Blog
+    {
+        public int Id { get; private set; }
+        public string Url { get; set; }
+
+        //use if you want row versioning.
+        public byte[] Timestamp { get; set; }
+    }
+
+  
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ProcessBlog();
+
+            var t1 = ProcessBlogConcurrent();
+            var t2 = ProcessBlogConcurrent();
+            await Task.WhenAll(t1, t2);
+        }
+
+        static async Task ProcessBlog()
+        {
+            using (var db = new BloggingContext())
+            //if you need explicit transaction support.
+            using (await db.Database.BeginTransactionAsync())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+
+                var count = await db.SaveChangesAsync();
+
+                var blogs = await db.Blogs.Where(b => !b.Url.EndsWith("/")).ToListAsync();
+
+                foreach (var blog in blogs)
+                {
+                    blog.Url += "/";
+                }
+
+                await db.SaveChangesAsync();
+
+                db.Database.CommitTransaction();
+            }
+        }
+
+        static async Task ProcessBlogConcurrent()
+        {
+            var random = new Random(Thread.CurrentThread.ManagedThreadId);
+            try
+            {
+                await ProcessBlog();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                await Task.Delay(random.Next(100));
+                await ProcessBlogConcurrent();
+            }
+        }
+    }
+}
---
+++ Section8/HttpRedir/HttpRedir/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+
+namespace HttpRedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var res = client.GetAsync("https://httpbin.org/redirect-to?url=https://google.com")
+                .Result.Content.ReadAsStringAsync()
+                .Result;
+            Console.WriteLine(res);
+
+        }
+    }
+}
